89ade966fafd1cce8168c3f085e0fbf2b1c7f1d8
Ryanu01 <ryangomes08090109@gmail.com>
2026-01-28 23:45:01 +0530

    Be

---
+++ backend/src/index.ts
@@ -0,0 +1,50 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 })
+
+interface User {
+  socket: WebSocket
+  roomId: string
+}
+
+let allSocket: User[] = []
+/*
+    '{
+        "type": "join",
+        "payload": {
+            "roomId": "123",
+            "message" : "user message"
+        }
+    }'
+*/
+
+wss.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    const parsedMessage = JSON.parse(message.toString())
+
+    console.log(typeof message.toString());
+
+    console.log(parsedMessage);
+
+    if (parsedMessage.type == "join") {
+      console.log("the type got entry");
+
+      allSocket.push({
+        socket,
+        roomId: parsedMessage.payload.roomId
+      })
+    }
+
+    console.log(allSocket.length)
+    if (parsedMessage.type == "chat") {
+      console.log("chat type got entry");
+      console.log(allSocket.length)
+
+      const curentUserRoom = allSocket.find(x => x.socket === socket)
+
+      const allUsersInRoom = allSocket.filter(x => x.roomId == curentUserRoom?.roomId)
+
+      allUsersInRoom.map(x => x.socket.send(parsedMessage.payload.message))
+    }
+  })
+})
